cc3ec56deefa560fffd77c650ff0cd832afa624d
pedroalves-dv <pedroalves.dv@gmail.com>
2026-04-15 20:24:17 +0100

    trying min-h on dropdown

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -459,7 +459,7 @@ export function Header({ isLoggedIn = false, customer }: HeaderProps) {
           style={{
             animation: `${isClosingMenu ? "navSlideUp" : "navSlideDown"} ${CLOSE_DURATION}ms ease-in-out forwards`,
           }}
-          className="sticky top-[var(--header-height)] bottom-0 left-0 right-0 z-[45] md:hidden px-site py-12 bg-white flex flex-col"
+          className="sticky top-[var(--header-height)] bottom-0 left-0 right-0 z-[45] md:hidden px-site py-12 bg-white flex flex-col section-min-h"
           aria-label="Mobile navigation"
         >
           <div className="group ">
